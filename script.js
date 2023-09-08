c0a7e6e3d05099722df8b1a4d431476e7c14e1cb
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-09-08 21:28:21 +0200

    Add game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const playerSelection = prompt("Player Selection").toLowerCase();
-const computerSelection = getComputerChoice();
+let playerPoints = 0;
+let computerPoints = 0;
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -16,19 +16,49 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("Tie");
+
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         console.log(`You Lose! Paper beats Rock`);
+        computerPoints++
+
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("You Won! Rock beats scissors");
+        playerPoints++
+
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("You Lose! Rock beats Paper");
+        computerPoints++
+
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("You Won! Paper beats scissors");
+        playerPoints++
+
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("You Lose! Rock beats scissors");
+        computerPoints++
+
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("You Won! Scissors beat Paper");
+        playerPoints++
+
     }
 };
 
-playRound(playerSelection, computerSelection);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Player Selection").toLowerCase();
+        const computerSelection = getComputerChoice();
+
+        playRound(playerSelection, computerSelection);
+    };
+
+    if (playerPoints === computerPoints) {
+        console.log("Tie")
+    } else if (playerPoints > computerPoints) {
+        console.log("You Won!")
+    } else {
+        console.log("You Lost!")
+    }
+};
+
+game();
