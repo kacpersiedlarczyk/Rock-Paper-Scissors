f6ac328e291e5d5a2bd21dea9ca32fb1e4f97c5b
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-09-08 20:27:10 +0200

    Add playRound

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const playerSelection = prompt("Player Selection").toLowerCase();
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
@@ -9,3 +12,23 @@ function getComputerChoice() {
         return "scissors"
     };
 };
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("Tie");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log(`You Lose! Paper beats Rock`);
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You Won! Rock beats scissors");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You Lose! Rock beats Paper");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("You Won! Paper beats scissors");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("You Lose! Rock beats scissors");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("You Won! Scissors beat Paper");
+    }
+};
+
+playRound(playerSelection, computerSelection);
